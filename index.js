9926062c6e89ab002326284935cb5c8df607fade
Tey García <tgarciamiranda@gmail.com>
2020-11-23 22:20:34 -0600

    password hashing

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const user = require('./controllers/user');
   await db.checkConnection()
   await db.sequelize.sync({ force: false })
 
-  await user.create()
-  await user.deleteOne(1)
-  await user.updateOne(2)
-  const users = await user.findAll()
-  console.log(users)
+  //await user.create()
+  //const loginResult = await user.login('me@tgarcia.com', 'khkkhk')
+  //console.log(loginResult)
+  // await user.deleteOne(1)
+  // await user.updateOne(2)
+  // const users = await user.findAll()
+  // console.log(users)
 
   await db.sequelize.close()
 })()
